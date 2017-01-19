98d05bbf756255194244d5d6be5930d5e209c67f
mrichins <richins.mason.a@gmail.com>
2017-01-19 14:19:18 -0600

    made testGoogle non-trivial

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,14 +1,17 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MyFirstTest {
 
     @Test
-    public void testGoogle(){
+    public void testGoogle() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com");
-        assert(driver.getCurrentUrl().contains("https://www.google.com"));
+        Thread.sleep(1000);
+        driver.findElement(By.name("q")).sendKeys("foo");
+        driver.findElement(By.name("btnG")).click();
         driver.quit();
     }
 }
